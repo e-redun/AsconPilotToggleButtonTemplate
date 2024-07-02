1b43328aa9cb32aea3067938ddb384ba6c370198
e-redun <e.redun@mail.ru>
2024-07-02 16:27:57 +0500

    Добавьте файлы проекта.

---
+++ Wpf_ToggleButton_Template/Plugins/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using Ascon.Pilot.SDK;
+using Ascon.Pilot.SDK.Menu;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Wpf_ToggleButton_Template
+{
+
+    [Export(typeof(IMenu<ObjectsViewContext>))]
+    public class Plugin : IMenu<ObjectsViewContext>
+    {
+        [ImportingConstructor]
+        public Plugin()
+        {
+            MessageBox.Show("Plugin call");
+        }
+
+        public void Build(IMenuBuilder builder, ObjectsViewContext context)
+        {
+            //MessageBox.Show("Build call");
+
+        }
+
+        public void OnMenuItemClick(string name, ObjectsViewContext context)
+        {
+            //MessageBox.Show("OnMenuItemClick call");
+
+        }
+    }
+}
